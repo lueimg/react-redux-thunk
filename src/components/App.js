b487932786bc6cbb211f1ed55315eb991cd5376c
luis mori <luismori@MacBook-Pro-de-luis.local>
2017-02-12 00:01:49 -0500

    base project

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <p> header here ... </p>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+App.propTypes = {
+    children: PropTypes.object.isRequired
+};
+
+export default App;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class AboutPage extends Component {
+    render () {
+        return (
+            <div>
+                <h1>About</h1>
+                <p> this application use react </p>
+            </div>
+        )
+    }
+}
+
+
+export default AboutPage;
+
+
