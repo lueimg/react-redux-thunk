096294323dad0551755763c573726fa509fb6296
luis mori <luismori@MacBook-Pro-de-luis.local>
2017-02-12 19:05:08 -0500

    adding handle error

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -33,7 +33,8 @@ class ManageCoursePage extends Component {
         this.setState({saving: true});
         this.props.actions.saveCourse(this.state.course).then(() => {
             this.redirect();
-        }, () => {
+        }).catch((error) => {
+            toastr.error(error);
             this.setState({saving: false});
         });
     }
